8eaeac8c9c9c6621479246d1f063edd7dd1e674f
Kabol Amine <amine.kabol@gmail.com>
2024-03-30 03:30:43 +0100

    hero section with css done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
@@ -9,41 +10,21 @@ const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="logo" />
-        <p >SHOPPER</p>
+        <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li
-          onClick={() => {
-            setMenu("shop");
-          }}
-        >
-          Shop{menu === "shop" ? <hr /> : <></>}
+        <li onClick={() => {setMenu("shop")}}><Link to='/'> Shop</Link>{menu === "shop" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("mens");
-          }}
-        >
-          Men{menu === "mens" ? <hr /> : <></>}
+        <li onClick={() => {setMenu("mens")}}><Link to='/mens'>Men</Link>{menu === "mens" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("womens");
-          }}
-        >
-          Women{menu === "womens" ? <hr /> : <></>}
+        <li onClick={() => {setMenu("womens")}}><Link to='/womens'>Women</Link>{menu === "womens" ? <hr /> : <></>}
         </li>
-        <li
-          onClick={() => {
-            setMenu("kids");
-          }}
-        >
-          Kids{menu === "kids" ? <hr /> : <></>}
+        <li onClick={() => {  setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
