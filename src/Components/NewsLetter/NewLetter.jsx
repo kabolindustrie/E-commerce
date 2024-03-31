a19b98da7e6cb13ba7529af7fba241fb4daeed1b
Kabol Amine <amine.kabol@gmail.com>
2024-03-31 16:17:14 +0200

    footer.jsx and footer.css created

--- src/Components/NewsLetter/NewLetter.jsx
+++ src/Components/NewsLetter/NewLetter.jsx
@@ -7,7 +7,7 @@ const NewLetter = () => {
       <h1>Get Exclusive Offers On Your Email</h1>
       <p>Subscribe to our newletter and stay updated</p>
       <div>
-        <input type="email" placeholder='Your Email id' />
+        <input type="email" placeholder='Your Email' />
         <button>Subscribe</button>
       </div>
     </div>
