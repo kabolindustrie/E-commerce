9a88d5287587ce029151be2a6207e5a6bcc88050
Kabol Amine <amine.kabol@gmail.com>
2024-04-02 18:39:15 +0200

    displayproduct.css

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -39,7 +39,7 @@ const ProductDisplay = (props) => {
         </div>
         <div className='productdisplay-right-size'>
           <h1>Select size</h1>
-          <div className="productdisplay-right-size">
+          <div className="productdisplay-right-sizes">
             <div>S</div>
             <div>M</div>
             <div>L</div>
