531ccff5f6e41bb1ab484c20ac163b0d59c248cf
Kabol Amine <amine.kabol@gmail.com>
2024-04-03 02:16:56 +0200

    descriptionbox.jsx and css // relatedProducts.jsx and css

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+    <div className="descriptionbox-description">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, illo? Laborum, beatae magni soluta reprehenderit, aperiam dolorum quas accusantium odio corporis non, dolores delectus! Itaque?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, aliquam consequatur. Ducimus ea, placeat at impedit cum debitis vitae quos sed voluptas amet sequi voluptatem.</p>
+    </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
